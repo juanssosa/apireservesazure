38ab0497c731c3e9b72274a7508900ee9f1a4abf
juanssosa <>
2023-10-03 22:48:25 -0500

    config.js changes

--- config.js
+++ config.js
@@ -7,26 +7,21 @@ const {
     PORT,
     HOST,
     HOST_URL,
-    DB_TYPE,
     DB_HOST,
     DB_PORT,
     DB_USER,
     DB_PASSWORD,
-    DB_NAME,
-    AUTO_LOAD_ENTITIES,
-    SYNCHRONIZE
+    DB_NAME
+
 } = process.env;
 
 module.exports = {
     port: PORT,
     host: HOST,
     url: HOST_URL,
-    dbType: DB_TYPE,
     dbHost: DB_HOST,
     dbPort: parseInt(DB_PORT),
     dbUser: DB_USER,
     dbPassword: DB_PASSWORD,
-    dbName: DB_NAME,
-    autoLoadEntities: AUTO_LOAD_ENTITIES === 'true',
-    synchronize: SYNCHRONIZE === 'true'
+    dbName: DB_NAME
 };
